2b4677da706c3698c0503807685017a7a83e5e40
Asistente Sistemas <pablomp250@gmail.com>
2020-12-17 15:35:05 -0500

    Add update function

--- PythonDrive/upload.py
+++ PythonDrive/upload.py
@@ -6,6 +6,9 @@ from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 
+# PATH_: path of project's folder
+# PATH: path of file's folder
+# FOLDER_NAME: folder's name in Google Driver
 PATH = '/Users/asistemas/Desktop/Txt/'
 PATH_ = '/Users/asistemas/Things/PythonDrive/'
 FOLDER_NAME = 'Txt'
@@ -46,9 +49,13 @@ def main():
         file.write("File log created        " + datetime.today().strftime('%Y/%m/%d %H:%M:%S') + os.linesep)
 
     # Search the folder Txt or created it.
-    results = service.files().list(q="mimeType='application/vnd.google-apps.folder'", pageSize=10, fields="nextPageToken, files(name, id)").execute() # pylint: disable=maybe-no-member
+    # List only folders  in drive account 
+    # and return foder's name and id.
+    results = service.files().list(q="mimeType='application/vnd.google-apps.folder' and trashed = false", pageSize=10, fields="nextPageToken, files(name, id)").execute() # pylint: disable=maybe-no-member
     items = results.get('files', [])
 
+    # If folder Txt do not exist, create it and return the id
+    # If the folder Txt exist only return the id
     idFolder = findFolder(items,FOLDER_NAME)
     if idFolder:
         file.write('Folder '+FOLDER_NAME+' found        ' + datetime.today().strftime('%Y/%m/%d %H:%M:%S') + os.linesep)
@@ -61,7 +68,39 @@ def main():
         fileDrive = service.files().create(body=file_metadata, fields='id').execute() # pylint: disable=maybe-no-member
         file.write('Folder '+FOLDER_NAME+' created whit ID: %s' % fileDrive.get('id') + '         ' + datetime.today().strftime('%Y/%m/%d %H:%M:%S') + os.linesep)
         idFolder = fileDrive.get('id')
+    file.close()
 
+    results = service.files().list(q="mimeType='text/plain' and trashed = false and '"+idFolder+"' in parents", pageSize=10, fields="nextPageToken, files(name, id)").execute() # pylint: disable=maybe-no-member
+    items = results.get('files', [])
+    
+    if items:
+        print(items)
+        updateFiles(idFolder,service,PATH,items)
+    else:
+        print('no files founded')
+        createFiles(idFolder,service,PATH)
+
+def updateFiles(idFolder,service,PATH,items):
+    filesToUpload = getFileToUpload()
+    for fileLocal, mimeType in filesToUpload:
+        fileExist = False
+        for item in items:
+            if item['name'] == fileLocal:
+                fileExist = True
+                metadata = {'name': fileLocal,
+                        'mimeType': mimeType,
+                        'addParents': [idFolder]
+                        }
+                res = service.files().update(fileId=item['id'], body=metadata, media_body=PATH+fileLocal).execute() # pylint: disable=maybe-no-member
+        if not fileExist:
+            metadata = {'name': fileLocal,
+                        'mimeType': mimeType,
+                        'parents': [idFolder]
+                        }
+            res = service.files().create(body=metadata, media_body=PATH+fileLocal).execute() # pylint: disable=maybe-no-member
+
+def createFiles(idFolder, service, PATH):
+    file = open(PATH_ + "log.log", "a")
     file.write('Preparing to upload files       ' + datetime.today().strftime('%Y/%m/%d %H:%M:%S') + os.linesep)
     filesToUpload = getFileToUpload()
     file.write('Files found to upload:' + ' '.join(map(str,filesToUpload)) + os.linesep)
@@ -75,6 +114,7 @@ def main():
         if res:
             file.write(res['name']+'    '+res['id']+ '  ' +res['mimeType']+'    '+ os.linesep) 
     file.write('Files created succesfully       ' + datetime.today().strftime('%Y/%m/%d %H:%M:%S') + os.linesep)
+    file.close()
 
 def findFolder(items, name):
     """ Searchs folder by name and return the ID of Folder
